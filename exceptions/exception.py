7f2867ea3f7330294f5f034eb9de6b1f904b1cc8
pvoanh <pvoanh@cmc.com.vn>
2019-12-06 10:03:55 +0700

    add exception

--- exceptions/exception.py
+++ exceptions/exception.py
@@ -1,6 +1,7 @@
 """
-+ woring exception in python
-
+    + woring exception in python
+    + except has contain many exception eg(except (RuntimeError, TypeError, NameError))
+    + 
 """
 
 def raise_exception():
@@ -12,6 +13,37 @@ def raise_exception():
             print("ooop! please try again ")
 
 
-raise_exception()
 
+
+def skip_exception(file): 
+    """
+    + # print error skip name of error on the last except
+    """
+    import sys
+    try:
+        f = open(file)
+        s = f.readline()
+        i = int(s.strip())
+        print('value of i ', i)
+    except OSError as err:
+        print("OS error: {0}".format(err))
+    except ValueError:
+        print("Could not convert data to an integer.")
+    except: 
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+
+
+def try_else():
+    """
+    + use else when try does not raise exception
     
+    """
+    pass
+
+if __name__ == "__main__":
+    # raise_exception()
+    # skip_exception('exception.txt')
+    try_else()
+    pass
