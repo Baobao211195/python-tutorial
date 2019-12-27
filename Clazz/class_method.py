e41b7bbb936bb1438839659a2dc598579ecdc529
pvoanh <pvoanh@cmc.com.vn>
2019-12-27 15:12:05 +0700

    add class method

---
+++ Clazz/class_method.py
@@ -0,0 +1,32 @@
+"""
+============== Class method =============================
+"""
+
+
+class Robot:
+
+    # create a private varible vs double underscores
+    __counter = 0
+
+    def __init__(self):
+        type(self).__counter += 1
+
+    @classmethod  # create class method
+    def robotInstances(cls):
+        return cls, Robot.__counter
+
+
+if __name__ == "__main__":
+    print(Robot.robotInstances())
+    x = Robot()
+    print(x.robotInstances())
+    y = Robot()
+    print(x.robotInstances())
+    print(Robot.robotInstances())
+
+    # print(Robot.robotInstances())
+    # x = Robot()
+    # print(x.robotInstances())
+    # y = Robot()
+    # print(y.robotInstances())
+    # print(Robot.robotInstances())
