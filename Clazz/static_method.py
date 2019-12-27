99cc992c68fa76603fd563c2aeaf6e9d2876c3c5
pvoanh <pvoanh@cmc.com.vn>
2019-12-27 14:47:42 +0700

    static method in python

---
+++ Clazz/static_method.py
@@ -0,0 +1,32 @@
+"""
+============== Static method =============================
+"""
+
+
+class Robot:
+
+    # create a private varible vs double underscores
+    __counter = 0
+
+    def __init__(self):
+        type(self).__counter += 1
+
+    @staticmethod
+    def robotInstances():
+        return Robot.__counter
+
+
+if __name__ == "__main__":
+    print(Robot.robotInstances())
+    x = Robot()
+    print(x.robotInstances())
+    y = Robot()
+    print(y.robotInstances())
+    print(Robot.robotInstances())
+
+    # print(Robot.robotInstances())
+    # x = Robot()
+    # print(x.robotInstances())
+    # y = Robot()
+    # print(y.robotInstances())
+    # print(Robot.robotInstances())
